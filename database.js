6b4535d923d6eb2c807c8eb0a33a252eb9d4dc72
Marco Rafael <ccristianoalves@hotmail.com>
2025-11-18 00:36:50 +0000

    Delete database.js

--- database.js
+++
@@ -1,7 +0,0 @@
-// database.js
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Conectado ao banco de dados'))
-  .catch((err) => console.log('Erro na conexão:', err));
